e096138875c608fad561720f650087cfc08be525
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-06-23 12:30:19 +0530

    ReactJs Code add

---
+++ Day2/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar  from './Component/Navbar'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Hello React Js</h1>
+    </>
+  )
+}
+
+export default App
---
+++ Day2/vite-project/src/Component/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar(){
+    return(
+        <>
+          <img src="" alt="Logo" />
+
+          <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact-us</a></li>
+            </ul>
+          </nav>
+        </>
+    )
+}
+
+export default Navbar
