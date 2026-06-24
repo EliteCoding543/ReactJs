9bea0d50679b4970218c66ce2d4cddbb832cebd1
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-06-24 17:53:15 +0530

    Update

--- Count-app/Count-App/src/Components/Navbar.jsx
+++ Count-app/Count-App/src/Components/Navbar.jsx
@@ -17,11 +17,11 @@ const Navbar = () => {
         }
       >
         <ul style={{display: "flex", listStyle: "none", gap: "20px"}}>
-            <li style={{textDecoration: "none"}}><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Service</a></li>
-            <li><a href="#">Contect-us</a></li>
-            <li><a href="#">Helps</a></li>
+            <li><a style={{textDecoration: "none", color: "black", fontSize: "20px",}} href="#">Home</a></li>
+            <li><a style={{textDecoration: "none", color: "black", fontSize: "20px",}} href="#">About</a></li>
+            <li><a style={{textDecoration: "none", color: "black", fontSize: "20px",}} href="#">Service</a></li>
+            <li><a style={{textDecoration: "none", color: "black", fontSize: "20px",}} href="#">Contect-us</a></li>
+            <li><a style={{textDecoration: "none", color: "black", fontSize: "20px",}} href="#">Helps</a></li>
         </ul>
 
         <button onClick={() => {
